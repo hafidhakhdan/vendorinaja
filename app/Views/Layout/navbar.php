0fff840e3e6ab70223e86457b4ac8b1044bfa41f
Difa Bagasputra Maulana <adif.maulana@gmail.com>
2020-11-12 01:14:52 +0700

    fix-structure

---
+++ app/Views/Layout/navbar.php
@@ -0,0 +1,19 @@
+    <nav>
+        <div class="logo">
+            <h1>Vendorinaja</h1>
+        </div>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Why Vendorinaja?</a></li>
+            <li><a href="">Features</a></li>
+            <li><a href="">Pricing</a></li>
+        </ul>
+        <button class="getstarted" data-toggle="modal" data-target="#exampleModal" data-whatever="@fat">Get Started</button>
+        
+        <div class="menu-toggle">
+            <input type="checkbox"/>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </nav>
--- app/Views/helpPage.php
+++ app/Views/helpPage.php
@@ -1,40 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- syarat fardhu a'in memulai ngoding web kalian adalah dengan memasukkan 3 fungsi ini (line 2-10) -->
+<?= $this->extend('/Layout/base'); ?>
+
+<?= $this->section('title');?>
     <title>Help Center --- Vendorin.aja</title>
-    <link rel="stylesheet" href="/assets/navbar.css">
+<?= $this->endSection('title'); ?>
+
+<?= $this->section('custom_css'); ?>
     <link rel="stylesheet" href="/assets/helpcenter.css">
-    <link rel="stylesheet" href="/assets/footer.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
-    <!-- NAVBAR FUNCTION -->
-    <nav>
-        <div class="logo">
-            <h1>Vendorinaja</h1>
-        </div>
+<?= $this->endSection('custom_css'); ?>
 
-        <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">Why Vendorinaja?</a></li>
-            <li><a href="">Features</a></li>
-            <li><a href="">Pricing</a></li>
-        </ul>
-
-        <div class="getstarted">
-            <a href="#">Get Started</a>
-        </div>
-
-        <div class="menu-toggle">
-            <input type="checkbox"/>
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-    </nav>
-
+<!-- section untuk kalian bermain dan belajar -->
+<?= $this->section('content'); ?>
     <!-- HELP CENTER -->
     <main class="main">
         <section id="main-content" class="section hero">
@@ -119,82 +95,4 @@
             </ul>
         </section>
     </main>
-    <footer class="footer">
-        <div class="footer-left">
-            <h2>Your Own Product.</h2>
-            <p>Your Own Design.</p>
-            <div class="line"></div>
-            <div class="socials">
-                <a href="" ><i class="fa fa-twitter"></i></a>
-                <a href="" ><i class="fa fa-instagram"></i></a>
-                <a href="" ><i class="fa fa-facebook"></i></a>
-                <a href="" ><i class="fa fa-youtube"></i></a>
-            </div>
-        </div>
-        <ul class="footer-right">
-            <li>
-                <h3>Product</h3>
-                <ul class="box">
-                    <li><a href="#">T-shirt</a></li>
-                    <li><a href="#">Kemeja</a></li>
-                    <li><a href="#">PDH</a></li>
-                </ul>
-            </li>
-            <li class="Company">
-                <h3>Company</h3>
-                <ul class="box">
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Jobs</a></li>
-                    <li><a href="#">Branding</a></li>
-                    <li><a href="#">Newsroom</a></li>
-                </ul>
-            </li>
-            <li class="features">
-                <h3>Resources</h3>
-                <ul class="box">
-                    <li><a href="#">Support</a></li>
-                    <li><a href="#">Safety</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li><a href="#">Feedback</a></li>
-                    <li><a href="#">Verification</a></li>
-                    <li><a href="#">Developers</a></li>
-                    <li><a href="#">StreamKit</a></li>
-                    <li><a href="#">Open Source</a></li>
-                    <li><a href="#">Security</a></li>
-                </ul>
-            </li>
-            <li class="features">
-                <h3>Policies</h3>
-                <ul class="box">
-                    <li><a href="#">Terms</a></li>
-                    <li><a href="#">Privacy</a></li>
-                    <li><a href="#">Guidlines</a></li>
-                    <li><a href="#">Acknowledgement</a></li>
-                    <li><a href="#">Licenses</a></li>
-                </ul>
-            </li>                     
-        </ul>
-        <div class="lineBottom"></div>
-        <div class="footer-bottom">
-            <h2>Vendorin.aja</h2>
-            <a href=""><button class="signup">Sign up</button></a>
-        </div>
-    </footer>
-        <script>
-        const menutoggle = document.querySelector('.menu-toggle input');
-        const nav = document.querySelector('nav ul');
-        const main = document.querySelector('.main')
-
-        menutoggle.addEventListener('click', function(){
-            nav.classList.toggle('slide');
-
-        })
-
-        main.addEventListener('click', function(){
-        	nav.classList.remove('slide');
-        })
-    </script>
-    <!-- /*<script type="text/javascript" id="statuspage-widget" statuspage="https://status.discord.com" src="//theme.zdassets.com/theme_assets/678183/03ab3d4309c01025141b76844d71d36556aeae67.js"></script>* -->
-
-</body>
-</html>
+<?= $this->endSection('content'); ?>
