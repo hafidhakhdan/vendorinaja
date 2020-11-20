79c96ea5b181f51f1696ca12d087b18b05499e4f
alfianfadhil <alvian.wadada@gmail.com>
2020-11-20 12:43:32 +0700

    landingPage responsive

--- app/Views/landingPage.php
+++ app/Views/landingPage.php
@@ -52,11 +52,11 @@
         </div>
         <div class="w-100"></div>
         <div class="col-lg-6 col-md-7 mt-5">
-            <h1 class="mt-5 ml-4">We sent it to <br> <span>your frontdoor!</span></h1>
-            <p class="mt-5 ml-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. A tempore doloremque, quae impedit, ipsa rerum dolorem eveniet est, harum voluptatibus ut illum dolor assumenda cumque expedita perspiciatis laboriosam reiciendis quibusdam?</p>
+            <h1 class="mt-5 ml-4 mr-5">We sent it to <br> <span>your frontdoor!</span></h1>
+            <p class="mt-5 ml-4 mr-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. A tempore doloremque, quae impedit, ipsa rerum dolorem eveniet est, harum voluptatibus ut illum dolor assumenda cumque expedita perspiciatis laboriosam reiciendis quibusdam?</p>
         </div>
         <div class="col-lg-6 col-md-5 section d-flex ">
-            <img class="ml-5" width="80%" src="/assets/img/landingPage/vector4.svg" alt="image1">
+            <img class="ml-3" width="80%" src="/assets/img/landingPage/vector4.svg" alt="image1">
         </div>
     </div>
     <!-- end of landing Page -->
